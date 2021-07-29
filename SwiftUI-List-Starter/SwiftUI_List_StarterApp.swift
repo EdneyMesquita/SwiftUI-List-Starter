41f2536e490f52e1dfbca7727fac472f76d6e0e1
EdneyMesquita <edneyphp@outlook.com>
2021-07-29 11:53:47 -0300

    Initial commit

---
+++ SwiftUI-List-Starter/SwiftUI_List_StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_List_StarterApp.swift
+//  SwiftUI-List-Starter
+//
+//  Created by Edney Mesquita on 29/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_List_StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            VideoListView()
+        }
+    }
+}
---
+++ SwiftUI-List-Starter/VideoDetailView.swift
@@ -0,0 +1,73 @@
+//
+//  VideoDetailView.swift
+//  SwiftUI-List-Starter
+//
+//  Created by Edney Mesquita on 29/07/21.
+//
+
+import SwiftUI
+
+struct VideoDetailView: View {
+    
+    var video: Video
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            
+            Image(video.imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 150)
+                .cornerRadius(12)
+            
+            Text(video.title)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+            
+            HStack(spacing: 40) {
+                Label("\(video.viewCount)", systemImage: "eye.fill")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                
+                Text(video.uploadDate)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+            
+            Text(video.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Link(destination: video.url, label: {
+                ButtonView(title: "Watch Now")
+            })
+        }
+    }
+}
+
+struct VideoDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoDetailView(video: VideoList.topTen.first!)
+    }
+}
+
+struct ButtonView: View {
+    
+    var title: String
+    
+    var body: some View {
+        Text(title)
+            .bold()
+            .font(.title2)
+            .frame(width: 280, height: 50)
+            .background(Color(.systemRed))
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+}
